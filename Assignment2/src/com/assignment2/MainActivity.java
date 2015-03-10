7a0dcd08ce67057cb71dc018317735c6ec6bf6a8
tmccartanIOS <rowing28>
2015-03-10 11:54:26 +0000

    Intital layouts - pre device specific layouts

--- Assignment2/src/com/assignment2/MainActivity.java
+++ Assignment2/src/com/assignment2/MainActivity.java
@@ -1,17 +1,58 @@
 package com.assignment2;
 
-import android.support.v7.app.ActionBarActivity;
+import java.util.ArrayList;
+
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+
 
 public class MainActivity extends ActionBarActivity {
 
+	static final String[] FRUIT = new String[] 
+			{"Banana", "Orange","Apple" };
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		ListView list = (ListView)findViewById(R.id.fruitList);
+		CustomAdapter adapter = new CustomAdapter(this,generateData());
+		list.setAdapter(adapter);
+		 
+		//ListView listView = getListView();
+		//listView.setTextFilterEnabled(true);
+		// Click event for single list row
+		
+		 
+	        // listening to single list item on click
+	      
+		list.setOnItemClickListener(new OnItemClickListener() {
+			 	@Override
+				public void onItemClick(AdapterView<?> parent, View view,
+						int position, long id) {
+			 		  Intent i = new Intent(getApplicationContext(), FruitDetail.class);
+		              // sending data to new activity
+			 		  Item selctedItem = (Item) parent.getItemAtPosition(position);
+		              i.putExtra("FRUIT", selctedItem );
+		              startActivity(i);
+				}
+			});
 	}
+   private ArrayList<Item> generateData(){
+        ArrayList<Item> items = new ArrayList<Item>();
+        items.add(new Item("Banana",R.drawable.banana_icon));
+        items.add(new Item("Orange", R.drawable.orange_icon));
+        items.add(new Item("Apple",R.drawable.apple_icon));
+ 
+        return items;
+    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
